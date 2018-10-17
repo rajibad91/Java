4a6fd832d5f0ff1540d102646a37cec6abb93ecc
rajibad91 <iamrajiba@gmail.com>
2018-10-17 10:10:22 +0530

    Add files via upload

---
+++ First/src/firstPackage/FirstClass.java
@@ -0,0 +1,13 @@
+package firstPackage;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("in main method");
+		//System.exit(0);
+		System.out.println("in main method");
+
+	}
+
+}
